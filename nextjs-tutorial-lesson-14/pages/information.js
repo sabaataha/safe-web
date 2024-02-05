4865c8d7bdb87696c83010ae65a7cc206502075d
ShaniYamin <shani@yamin.co.il>
2024-02-05 11:15:07 +0200

    create info page

---
+++ nextjs-tutorial-lesson-14/pages/information.js
@@ -0,0 +1,21 @@
+const Information = () => {
+    return (
+      <>
+        <Head>
+          <title>Ninja List | Information</title>
+          <meta name="keywords" content="ninjas"/>
+        </Head>
+        <div>
+          <h1>Information</h1>
+          <p>Welcome to our comprehensive educational game, where learning about sexual education is not only informative but also engaging.<br/>
+            Dive into different facets of understanding with our Quiz Game, testing your knowledge; navigate real-life situations in our Scenario Challenge, 
+            promoting critical thinking; join the open dialogue in our Discussion Forum for healthy conversations; 
+            embark on an interactive journey with our Story Mode, and track your progress with insightful Statistics that provide valuable feedback.<br/>
+            This immersive experience aims to make learning both enlightening and enjoyable, fostering a more informed and empowered you.
+          </p>        
+        </div>
+      </>
+    );
+  }
+   
+  export default Information;
