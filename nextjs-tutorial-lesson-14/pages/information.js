288f196c05e191f7af8e489fac1c0bd550096068
ShaniYamin <shani@yamin.co.il>
2024-02-05 11:36:05 +0200

    edit info page and add info icon

--- nextjs-tutorial-lesson-14/pages/information.js
+++ nextjs-tutorial-lesson-14/pages/information.js
@@ -1,3 +1,8 @@
+import Head from 'next/head'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+
 const Information = () => {
     return (
       <>
@@ -6,11 +11,12 @@ const Information = () => {
           <meta name="keywords" content="ninjas"/>
         </Head>
         <div>
-          <h1>Information</h1>
+        <h1><FontAwesomeIcon icon={faCircleInfo} /> Information</h1>
           <p>Welcome to our comprehensive educational game, where learning about sexual education is not only informative but also engaging.<br/>
-            Dive into different facets of understanding with our Quiz Game, testing your knowledge; navigate real-life situations in our Scenario Challenge, 
-            promoting critical thinking; join the open dialogue in our Discussion Forum for healthy conversations; 
-            embark on an interactive journey with our Story Mode, and track your progress with insightful Statistics that provide valuable feedback.<br/>
+            Dive into different facets of understanding with our quiz game, testing your knowledge, navigate real-life situations in our scenario challenge,
+            promoting critical thinking.<br/>
+            Join the open dialogue in our discussion forum for healthy conversations, embark on an interactive journey with our story mode, 
+            and track your progress with insightful statistics that provide valuable feedback.<br/>
             This immersive experience aims to make learning both enlightening and enjoyable, fostering a more informed and empowered you.
           </p>        
         </div>
